afc00315e2b2ca0fe3ea4d79aa9a04bb032e7194
Henry Salazar <hello369@live.com>
2020-02-19 10:36:25 -0500

    more html

--- reimbursement/src/main/java/com/revature/reimbursement/servlets/LoginServlet.java
+++ reimbursement/src/main/java/com/revature/reimbursement/servlets/LoginServlet.java
@@ -4,9 +4,14 @@ import com.revature.reimbursement.User;
 import java.sql.SQLException;
 import com.revature.reimbursement.exceptions.InvalidLoginException;
 import com.revature.reimbursement.exceptions.ConnectionException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.revature.reimbursement.ConnectionUtil;
 import com.revature.reimbursement.Data;
 import com.revature.reimbursement.daos.UserDAO;
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpServletRequest;
@@ -15,27 +20,58 @@ import javax.servlet.http.HttpServlet;
 public class LoginServlet extends HttpServlet
 {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.getWriter().write("{\"doGet\":\"response\"}");
+        
+    	//TESTING UserDAO
+    	
+    	UserDAO userDAO = new UserDAO();
+        User user;
+		try {
+			user = userDAO.logIn("henry", "password");
+
+	    	response.getWriter().write("Success");
+	    	return;
+		} catch (ConnectionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidLoginException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    	response.getWriter().write("failed"}");
     }
     
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String username = request.getParameter("username");
+        
+    	//unmarshalling. creating java from JSON
+//    	ObjectMapper om = new ObjectMapper();
+//    	om.readValue(request.getReader(), User.class);
+    	   	
+    	String username = request.getParameter("username");
         String password = request.getParameter("password");
         UserDAO userDAO = new UserDAO();
         try {
             User user = userDAO.logIn(username, password);
             Data.getData().setUser(user);
-            
-            response.getWriter().write("{\"doPost\":\"response\"}");
+                        
+            response.setStatus(ConnectionUtil.STATUS_SUCCESS);
+            response.setContentType("application/json");
+
+            //marshalling
+        	ObjectMapper om = new ObjectMapper();
+        	om.writeValue(response.getWriter(), user);
         }
         catch (ConnectionException e) {
-            response.setStatus(503);
+            response.setStatus(ConnectionUtil.STATUS_UNAVAILABLE);
         }
         catch (InvalidLoginException e2) {
-            response.setStatus(401);
+            response.setStatus(ConnectionUtil.STATUS_UNAUTHORIZED);
         }
         catch (SQLException e3) {
-            response.setStatus(500);
+            response.setStatus(ConnectionUtil.STATUS_SERVER_ERROR);
         }
     }
 }
--- reimbursement/src/main/webapp/shared.js
+++ reimbursement/src/main/webapp/shared.js
@@ -24,6 +24,45 @@ function getStatus(statusId) {
     return "Unknown";
 }
 
+//postParams should be an object literal
+async function postRequest(postParams, url, callback) {
+	try {
+		const config = {
+			method: 'POST',
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(postParams)
+		}
+
+        const response = await fetch(url, config);
+        
+        //if error
+        if(response.status >= 400) {
+
+            let errorMessage;
+            switch(response.status) {
+                case 401: errorMessage = "Unauthorized Error"; break;
+                case 500: errorMessage = "Server Error"; break;
+                case 503: errorMessage = "Database Connection Error"; break;
+                default: errorMessage = "Error";
+            }
+
+            callback({}, errorMessage);
+            return;
+        }
+
+        let json = await response.json()
+        
+        callback(json, "");
+	}
+	catch (error) {
+
+        callback({}, "Error");
+	}
+}
+
 //set the table cell
 //tr is <tr> element. data is cell data
 function setTableCell(tr, data) {
