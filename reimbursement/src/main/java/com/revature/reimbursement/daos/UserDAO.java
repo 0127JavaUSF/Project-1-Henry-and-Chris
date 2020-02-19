dec9927db7dae5f90f71e25cb3dada6edd97e0c2
Henry Salazar <hello369@live.com>
2020-02-19 10:59:43 -0500

    added hash function

--- reimbursement/src/main/java/com/revature/reimbursement/daos/UserDAO.java
+++ reimbursement/src/main/java/com/revature/reimbursement/daos/UserDAO.java
@@ -6,6 +6,10 @@ import java.sql.PreparedStatement;
 import java.sql.Connection;
 import java.sql.SQLException;
 import com.revature.reimbursement.exceptions.InvalidUserException;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import at.favre.lib.crypto.bcrypt.BCrypt.Hasher;
+
 import com.revature.reimbursement.exceptions.ConnectionException;
 import com.revature.reimbursement.ConnectionUtil;
 import com.revature.reimbursement.User;
@@ -75,6 +79,31 @@ public class UserDAO implements IUserDAO
         }
         catch (SQLException e) {
             throw e;
+        }
+    }
+    
+    public void hashDatabasePasswords() throws ConnectionException, SQLException
+    {
+    	Hasher hash = BCrypt.withDefaults();
+    	try(Connection connection = ConnectionUtil.getConnection()) {
+            if (connection == null) {
+                throw new ConnectionException();
+            }
+            connection.setAutoCommit(false);            String sql = "SELECT ers_password FROM ers_users WHERE ers_user_id = ?;";            PreparedStatement prepared = connection.prepareStatement(sql);
+            prepared.setInt(1, 3);
+            ResultSet result = prepared.executeQuery();
+            if (result.next()) {                char[] password = result.getString("ers_password").toCharArray();
+                byte[] newPass = hash.hash(12,password);                String sql2 = "UPDATE ers_users "
+                		+ "set ers_password = ? "
+                		+ "where ers_user_id = ?";
+                PreparedStatement statement2 = connection.prepareStatement(sql2);				statement2.setString(1, newPass.toString());
+				statement2.setInt(2, 3);				statement2.executeUpdate();
+				System.out.println("Hashing id 2 password to " + newPass.toString());
+				connection.commit();
+            }
+        }
+        catch (SQLException e) {
+            throw new SQLException();
         }
     }
 }
